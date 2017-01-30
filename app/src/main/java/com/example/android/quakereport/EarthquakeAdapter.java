8cdf7d65cc7cf297b421cbc69b69657c24d12551
Darren Lee <dtg472@gmail.com>
2017-01-29 23:01:10 -0800

    Update to Lesson 1, Quiz: Circle background for magnitude

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,7 +1,10 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.graphics.drawable.RotateDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import android.graphics.drawable.GradientDrawable;
 
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Set;
 
 /**
  * Array Adapter to display Earthquake class data in a listview
@@ -36,8 +41,15 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         Earthquake earthquake = getItem(position);
 
         //Setup the magnitude
+        //Get the view
         TextView magnitudeText = (TextView) convertView.findViewById(R.id.magnitude);
+        //Set the text
         magnitudeText.setText(String.format(Locale.US, "%.1f", earthquake.getMagnitude()));
+        //Find the circle and change color
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeText.getBackground();
+        int magnitudeColor = getMagnitudeColor(earthquake.getMagnitude());
+        magnitudeColor = ContextCompat.getColor(getContext(), magnitudeColor);
+        magnitudeCircle.setColor(magnitudeColor);
 
         //Setup the location data
         String fullLocation = earthquake.getLocation();
@@ -84,5 +96,38 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private String formatTime(Date dateObject) {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
         return timeFormat.format(dateObject);
+    }
+
+    /**
+     * Returns a color for different magnitude
+     * @param magnitude
+     * @return a color
+     */
+    private int getMagnitudeColor(double magnitude){
+        int intMagnitude = (int) magnitude; //convert to int without rounding
+        switch (intMagnitude){
+            case 0:
+            case 1:
+                return R.color.magnitude1;
+            case 2:
+                return R.color.magnitude2;
+            case 3:
+                return R.color.magnitude3;
+            case 4:
+                return R.color.magnitude4;
+            case 5:
+                return R.color.magnitude5;
+            case 6:
+                return R.color.magnitude6;
+            case 7:
+                return R.color.magnitude7;
+            case 8:
+                return R.color.magnitude8;
+            case 9:
+                return R.color.magnitude9;
+            case 10:
+            default:
+                return R.color.magnitude10plus;
+        }
     }
 }
